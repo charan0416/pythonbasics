e6ce2fe6e813d0f184d31c16db374b2554f5f76e
charan0416 <charanbaru169@gmail.com>
2025-02-21 12:57:34 +0530

    numbers123

--- lists.py
+++ lists.py
@@ -6,5 +6,12 @@ l1.insert(2,"orange")
 print(l1)
 l1.pop(2)
 l1.remove(2)
-l1.reverse
+l1.reverse()
 print(l1)
+l3 = [34,51,61,99,[52,4,5,65],('34','45','477',"charan"),[36,77,66],[47,54,56,44]]
+print(l3)
+x = l3.copy()
+print(x)
+#shallowcopy and deepcopy
+
+
