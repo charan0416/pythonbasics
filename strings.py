affc8c258f8160e8ee40f273a06fd0c46bfb3d4c
charan0416 <charanbaru169@gmail.com>
2025-02-21 15:15:37 +0530

    strngs

---
+++ strings.py
@@ -0,0 +1,12 @@
+s1 = "koushikou"
+t = "1"
+d = s1 + t
+print(d)
+count = s1.count("k")
+print(count)
+lenght = len(s1)
+print(lenght)
+print(s1.upper())
+print(s1.lower())
+print(s1.count("kou"))
+
