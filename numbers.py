c2c7cc5cf84ab17a89afc374504935c8e81649f5
charan0416 <charanbaru169@gmail.com>
2025-02-21 10:46:22 +0530

    numbers

---
+++ numbers.py
@@ -0,0 +1,15 @@
+x = 189
+y =293
+print(min(x,y))
+print(max(x,y))
+print(float(x))
+print(bool(x))
+print(bytes(x))
+print(x+y)
+print(x,y)
+print(complex(x))
+print(bin(x))
+z = 2.0
+print(int(z))
+print(type(z))
+print(type(x))
